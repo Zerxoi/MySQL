fcbbfc9f09ac8719f8d23fe0226d4e6f24165bd7
zerxoi <zerxoi@163.com>
2021-03-26 12:30:01 +0800

    SQL格式调整

--- demo.sql
+++ demo.sql
@@ -3,13 +3,13 @@ DROP TABLE IF EXISTS DEPT;
 DROP TABLE IF EXISTS SALGRADE;
 
 CREATE TABLE DEPT
-       (DEPTNO int(2) not null ,
+	   (DEPTNO int(2) not null ,
 	DNAME VARCHAR(14) ,
 	LOC VARCHAR(13),
 	primary key (DEPTNO)
 	);
 CREATE TABLE EMP
-       (EMPNO int(4)  not null ,
+	   (EMPNO int(4)  not null ,
 	ENAME VARCHAR(10),
 	JOB VARCHAR(9),
 	MGR INT(4),
@@ -18,13 +18,13 @@ CREATE TABLE EMP
 	COMM DOUBLE(7,2),
 	primary key (EMPNO),
 	DEPTNO INT(2) 
-	)
-	;
+	);
 
-CREATE TABLE SALGRADE
-      ( GRADE INT,
+CREATE TABLE SALGRADE (
+	GRADE INT,
 	LOSAL INT,
-	HISAL INT );
+	HISAL INT
+);
 
 
 
--- 存储过程.sql
+++ 存储过程.sql
@@ -28,10 +28,10 @@ drop procedure hello_procedure//
 create procedure sp_var01()
 begin
 	-- 局部变量 begin end 块内容有效
-    -- 需要先使用 declare 声明
+	-- 需要先使用 declare 声明
 	declare var01 varchar(32) default 'sf';
-    set var01 = 'ug';
-    select var01;
+	set var01 = 'ug';
+	select var01;
 end//
 
 drop procedure sp_var01//
@@ -39,8 +39,8 @@ drop procedure sp_var01//
 create procedure sp_var01()
 begin
 	declare var01 varchar(32) default 'sf';
-    select var01;
-    set var01 = 'ug';
+	select var01;
+	set var01 = 'ug';
 end//
 
 -- begin end 块外获取不到该变量
@@ -102,7 +102,7 @@ set @param03 = 333;
 create procedure sp_param03(in in_param int, inout inout_param int)
 begin
 	set inout_param = 666;
-    set inout_param = inout_param + in_param;
+	set inout_param = inout_param + in_param;
 end;
 
 call sp_param03(333 ,@param03)//
@@ -143,7 +143,7 @@ begin
 		when score >= 80 then set grade = 'B';
 		when score >= 70 then set grade = 'C';
 		when score >= 60 then set grade = 'D';
-        else set grade = 'E';
+		else set grade = 'E';
 	end case;
 end//
 
@@ -165,13 +165,13 @@ drop procedure sp_loop//
 create procedure sp_loop()
 begin
 	declare num_index int default 0;
-    num_loop:loop
+	num_loop:loop
 		select num_index;
 		set num_index = num_index + 1;
 		if num_index < 10 then
 			iterate num_loop;  --  类似continue
 		end if;
-        leave num_loop; -- 类似 break
+		leave num_loop; -- 类似 break
 	end loop num_loop;
 end//
 
@@ -182,12 +182,12 @@ drop procedure sp_repeat//
 create procedure sp_repeat()
 begin
 	declare num_index int default 0;
-    num_loop:repeat
+	num_loop:repeat
 		-- 语句开始
 		select num_index;
 		set num_index = num_index + 1;
-        -- 语句结束
-        until num_index >= 10
+		-- 语句结束
+		until num_index >= 10
 	end repeat num_loop;
 end//
 
@@ -202,7 +202,7 @@ begin
 		-- 语句开始
 		select num_index;
 		set num_index = num_index + 1;
-        -- 语句结束
+		-- 语句结束
 	end while;
 end//
 
@@ -217,27 +217,27 @@ drop procedure sp_cusor_handler//
 create procedure sp_cusor_handler()
 begin
 	declare id_param int;
-    declare author_param varchar(32);
-    declare flag boolean default true;
+	declare author_param varchar(32);
+	declare flag boolean default true;
 	declare comment_cursor cursor for
 		select 
 			id, author 
-        from 
+		from 
 			jandan_pic.comment
 		where 
 			id between 4753197 and 4753250;
 
 	-- 游标取出最后一个元素会抛出1329的错误，通过handler句柄捕捉错误
-    -- 将flag设为false，并在出错的语句继续执行
+	-- 将flag设为false，并在出错的语句继续执行
 	declare continue handler for 1329 set flag = false;
 	open comment_cursor;
-    	while flag do
+		while flag do
 			-- 语句开始
 			fetch comment_cursor into id_param, author_param;
 			select author_param;
 			-- 语句结束
 		end while;
-    close comment_cursor;
+	close comment_cursor;
 end//
 
 call sp_cusor_handler()//
@@ -250,46 +250,46 @@ create procedure sp_create_table()
 begin
 	begin
 		declare next_year int;
-        declare next_month int;
-        declare next_month_str char(2);
-        declare next_month_day int;
+		declare next_month int;
+		declare next_month_str char(2);
+		declare next_month_day int;
 		declare next_month_day_str varchar(2);
-        declare table_name_str char(10);
-        declare t_index int default 1;
+		declare table_name_str char(10);
+		declare t_index int default 1;
 		
-        set next_year = year(date_add(now(), interval 1 month));
-        set next_month = month(date_add(now(), interval 1 month));
-        set next_month_day = dayofmonth(last_day(date_add(now(), interval 1 month)));
-        
-        if next_month < 10 then
+		set next_year = year(date_add(now(), interval 1 month));
+		set next_month = month(date_add(now(), interval 1 month));
+		set next_month_day = dayofmonth(last_day(date_add(now(), interval 1 month)));
+		
+		if next_month < 10 then
 			set next_month_str = concat('0', next_month);
 		else
 			set next_month_str = concat('', next_month);
 		end if;
-        
-        while t_index <= next_month_day do
+		
+		while t_index <= next_month_day do
 			if t_index < 10 then
 				set next_month_day_str = concat('0', t_index);
-                -- if t_index = 1 then
-					select next_month_day_str;
+				-- if t_index = 1 then
+					SELECT next_month_day_str;
 				-- end if;
 			else
 				set next_month_day_str = concat('', t_index);
 			end if;
-            
-            
+			
+			
 			set table_name_str = concat(next_year,'_', next_month_str,'_', next_month_day_str);
-            set @create_table_sql = concat(
+			set @create_table_sql = concat(
 				'create table comp_',
-                table_name_str,
-                '(`grade` int, `losal` int, `hisal` int)'
-            );
-            prepare create_table_stmt from @create_table_sql;
-            execute create_table_stmt;
-            deallocate prepare create_table_stmt;
-            set t_index = t_index + 1;
-        end while;
-    end;
+				table_name_str,
+				'(`grade` int, `losal` int, `hisal` int)'
+			);
+			prepare create_table_stmt from @create_table_sql;
+			execute create_table_stmt;
+			deallocate prepare create_table_stmt;
+			set t_index = t_index + 1;
+		end while;
+	end;
 end;
 
 call sp_create_table()//
